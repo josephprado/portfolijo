2088aa1de8884083f918c308c0a570574222bd8c
prado <pradodje@gmail.com>
2023-03-17 23:43:48 -0700

    Add emotion and base app layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,61 @@
+import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
 
 function App() {
-  return <Outlet />;
+  const headerHeight = 48;
+
+  return (
+    <Container headerHeight={headerHeight}>
+      <Header height={headerHeight}>
+        <Navbar>Nav</Navbar>
+      </Header>
+      <ScrollContainer>
+        <Main>
+          <Outlet />
+        </Main>
+        <Footer>Footer</Footer>
+      </ScrollContainer>
+    </Container>
+  );
 }
+
+const Container = styled.div<{ headerHeight: number }>`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  padding-top: ${(props) => `${props.headerHeight}px`};
+`;
+
+const Header = styled.header<{ height: number }>`
+  position: fixed;
+  top: 0;
+  display: flex;
+  width: 100%;
+  height: ${(props) => `${props.height}px`};
+  background-color: grey;
+`;
+
+const Navbar = styled.nav`
+  display: flex;
+`;
+
+const ScrollContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+`;
+
+const Main = styled.main`
+  flex-grow: 1;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  background-color: grey;
+`;
 
 export default App;
