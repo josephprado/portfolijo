ee2d1044901dd2bbb9a79158841509202b209925
prado <pradodje@gmail.com>
2023-03-25 10:12:49 -0700

    Move app inside components folder

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
-import Header from './components/header/Header';
+import Header from './header/Header';
 
 function App() {
   return (
